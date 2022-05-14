416d80fa04346858a290ca3367a02113753407ba
Jeb Feng <ranzeplay@outlook.com>
2022-05-14 19:20:03 +0800

    Webpage can request server for player list on webpage loaded

--- mc_server_dashboard/web_server.py
+++ mc_server_dashboard/web_server.py
@@ -1,8 +1,8 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 from typing import Optional, Tuple, List
+import json
 
 from mcdreforged.api.all import *
-from minecraft_version_api import *
 
 from .config_manager import get_config
 
@@ -10,6 +10,7 @@ global server
 global data_api
 global config
 global webpage_content
+
 
 @new_thread('WebServer')
 def setup_server(instance: PluginServerInterface):
@@ -31,6 +32,7 @@ def setup_server(instance: PluginServerInterface):
     server.serve_forever()
 
 
+@new_thread
 def stop_server():
     print('Shutting down web server')
     server.shutdown()
@@ -44,9 +46,12 @@ class HTTPRequestHandler(SimpleHTTPRequestHandler):
             self.get_server_name()
         elif self.path == '/server-version':
             self.get_server_version()
+        elif self.path == '/players':
+            self.get_players()
         else:
             # Return webpage
             self.get_page()
+        pass
 
     def get_server_name(self):
         global config
@@ -54,6 +59,7 @@ class HTTPRequestHandler(SimpleHTTPRequestHandler):
         self.send_header('Content-Type', 'text/plain; charset=utf-8')
         self.end_headers()
         self.wfile.write(config.server_name.encode())
+        pass
 
     def get_server_version(self):
         global config
@@ -61,21 +67,37 @@ class HTTPRequestHandler(SimpleHTTPRequestHandler):
         self.send_header('Content-Type', 'text/plain; charset=utf-8')
         self.end_headers()
         self.wfile.write(config.server_version.encode())
+        pass
 
     def get_page(self):
         self.send_response(200)
         self.send_header('Content-Type', 'text/html; charset=utf-8')
         self.end_headers()
         self.wfile.write(webpage_content.encode())
+        pass
+
+    def get_players(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/json; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(get_player_list().encode())
+        pass
 
 
 # Get player count
-def get_player_list(mpa):
-    query_result: Optional[Tuple[int, int, List[str]]] = mpa.get_server_player_list()
+def get_player_list():
+    query_result: Optional[Tuple[int, int, List[str]]] = data_api.get_server_player_list()
     if query_result is not None:
         current_player_count = query_result[0]
-        max_players = query_result[1]
+        max_player_count = query_result[1]
         player_list = query_result[2]
+        return json.dumps({
+            'playerList': player_list,
+            'maxPlayerCount': max_player_count,
+            'currentPlayerCount': current_player_count
+        })
+
+    return "{}"
 
 
 def get_webpage(instance: PluginServerInterface) -> str:
